4fd9e67221256a78beca1de236fe42070cc534ad
Pekka Helenius <pekka.helenius@fjordtek.com>
2022-06-29 19:09:57 +0300

    Disable BPF offload

--- lineage_src_root/device/samsung/s5neolte/device.mk
+++ lineage_src_root/device/samsung/s5neolte/device.mk
@@ -124,6 +124,10 @@ PRODUCT_COPY_FILES += \
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += $(LOCAL_PATH)/overlay
 
+# Tethering
+PRODUCT_PACKAGES += \
+    TetheringConfigOverlay
+
 # Vendor security patch level
 PRODUCT_PROPERTY_OVERRIDES += \
     ro.lineage.build.vendor_security_patch=2017-03-01
